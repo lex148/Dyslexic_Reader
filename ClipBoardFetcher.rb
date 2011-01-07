bd5d899df4767aa0f7fc0290d91689e46f48f38d
Lex Childs <lex.childs@praeses.com>
2011-01-07 11:01:48 -0600

    update ignore to remove -

--- ClipBoardFetcher.rb
+++ ClipBoardFetcher.rb
@@ -5,5 +5,13 @@ $LOAD_PATH.unshift "."
 require 'gtk2'
 
 source = Gtk::Clipboard.get(Gdk::Selection::CLIPBOARD)
+badchars = %w"' –"
+
 # wait for clipboard content
-puts source.wait_for_text.gsub("'","")
+text = source.wait_for_text
+
+badchars.each do |c|
+  text = text.gsub(c, "")
+end
+
+puts text
