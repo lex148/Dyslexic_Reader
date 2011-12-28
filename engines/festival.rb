915792171e76012277868dc6fe8e1b330f4ad1b8
Lex Childs <lexchilds@gmail.com>
2011-12-28 10:53:09 -0600

    update to use pipe, reads immediately

--- engines/festival.rb
+++ engines/festival.rb
@@ -8,56 +8,35 @@ class Festival
     text = clean text
     `rm .temp.txt`
     `touch .temp.txt`
-
     aFile = File.new(".temp.txt", "w")
     aFile.write(text)
     aFile.close 
     voice = "voice_cmu_us_slt_arctic_hts"
-    command = "text2wave -eval \"(#{voice})\" -o .temp.wav .temp.txt\n"
-    #command = "echo \"" + text + "\" | text2wave -o .temp.wav \n"
-    # this is blocking, wait for wav file to be created
+    command = "text2wave -eval \"(#{voice})\" -o /dev/stdout .temp.txt |lame - - | vlc - vlc://quit"
     outputText = `#{command}`
     puts outputText
-    #IO.popen command
-    readStr = "vlc .temp.wav vlc://quit"
-    #readStr = "cvlc .temp.wav vlc://quit"
-    puts readStr
-    @readerProssess = IO.popen readStr
-
-  if (File.exist? ".temp.wav") == false
+    if (File.exist? ".temp.wav") == false
       logReadError text
-  end
+    end
   end
 
   def stop()
-    if(@readerProssess != nil and @readerProssess.closed? == false)
-    command = "kill " + (@readerProssess.pid).to_s
-    # this is blocking, wait for wav player to be killed
-    outputText = `#{command}`
-    puts outputText
-  end 
-  puts "Festival stop"
-  if File.exist? ".temp.wav"
-    puts "delete .temp.wav"
-    File.delete ".temp.wav"
-  end 
   end
 
   def clean(text)
-  text = text.delete "\""
+    text = text.delete "\""
   end
 
   def logReadError(text)
-  puts "Logging Error"
-  filename = "swiftError.log"
-  if File.exist? filename
-    logfile = File.new(filename, "w")
-    logfile.puts "\n\nErrorReading:" + text
+    puts "Logging Error"
+    filename = "swiftError.log"
+    if File.exist? filename
+      logfile = File.new(filename, "w")
+      logfile.puts "\n\nErrorReading:" + text
     else
       logfile = File.open(filename, "a")
-    logfile.puts "\n\nErrorReading:" + text
-  end
-    
+      logfile.puts "\n\nErrorReading:" + text
+    end
   end
 
 end
