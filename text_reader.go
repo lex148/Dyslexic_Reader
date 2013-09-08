3eea43c2e3e480fe56cd8a407e732e9113c850fd
Lex Childs <lexchilds@gmail.com>
2013-09-08 08:21:17 -0500

    cleanup to use xevents

--- text_reader.go
+++ text_reader.go
@@ -1,67 +1,51 @@
 package main
 
-import "fmt"
-import "os"
-import "os/exec"
-import "time"
-import "github.com/gvalkov/golang-evdev"
+import (
+  "fmt"
+  "os/exec"
+	"github.com/BurntSushi/xgbutil"
+	"github.com/BurntSushi/xgbutil/keybind"
+	"github.com/BurntSushi/xgbutil/xevent"
+)
 
 
 func main() {
 
-  var events []evdev.InputEvent
-  devs,_ := evdev.ListInputDevices("/dev/input/by-id/*Keyboard*")
-  first := devs[0]
-  dev ,err := evdev.Open(first)
-
-  if(err != nil){
-    fmt.Println("************************")
-    fmt.Println(err)
-    fmt.Println("************************")
-    os.Exit(1)
-  }
-
+  X, _ := xgbutil.NewConn()
+	keybind.Initialize(X)
+	cb1 := keybind.KeyPressFun(
+		func(X *xgbutil.XUtil, e xevent.KeyPressEvent) {
+      readit()
+		})
+	cb1.Connect(X, X.RootWin(), "control-z", true)
 
   fmt.Printf("Listening for Crtl-z ...\n")
-  ctrl := false
-  z := false
-
-  for {
-    events, err = dev.Read()
-    for i := range events {
-      if( events[i].Value == 0x0 && events[i].Code == 29){ ctrl = false } //ctrl up
-      if( events[i].Value == 0x1 && events[i].Code == 29){ ctrl = true } //ctrl down
-      if( events[i].Value == 0x1 && events[i].Code == 44){ z = true  }
-    }
-    if( ctrl && z ){ readit() }
-    z = false
-    time.Sleep(500)
-  }
+	xevent.Main(X)
 
 }
 
 
-var vlc (exec.Cmd)
+var player (exec.Cmd)
 
 func readit( ){
 
-  if( vlc.Process != nil ){
-    vlc.Process.Kill()
-    vlc.Process.Wait()
+  if( player.Process != nil ){
+    player.Process.Kill()
+    player.Process.Wait()
   }
 
   xsel := exec.Command("xsel")
   festival := exec.Command("text2wave","-o", "/dev/stdout","/dev/stdin" )
-  vlc = *exec.Command("cvlc","stream:///dev/stdin","vlc://quit" )
+  player = *exec.Command("aplay","/dev/stdin" )
 
   xsel_pipe,_ := xsel.StdoutPipe()
   festival.Stdin = xsel_pipe
   festival_pipe,_ := festival.StdoutPipe()
-  vlc.Stdin = festival_pipe
+  player.Stdin = festival_pipe
 
   xsel.Start()
   festival.Start()
-  vlc.Start()
+  player.Start()
 
 }
 
